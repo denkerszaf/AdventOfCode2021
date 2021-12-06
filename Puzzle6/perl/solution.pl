768b44f44335e153292ffcbd69eadf79e4defd8b
Stefan Denker <Stefan@dn-kr.de>
2021-12-06 23:40:40 +0100

    Solution part 2

--- Puzzle6/perl/solution.pl
+++ Puzzle6/perl/solution.pl
@@ -4,38 +4,82 @@ use strict;
 use warnings; 
 use 5.32.0;
 
-sub decrease {
-	my ($fish) = @_;
+sub histogram {
+	my ($input) = @_;
 	
-	if ($fish > 0) {
-		return $fish -1 ;
+	my %result = ();
+	
+	foreach my $value (split ',', $input) {
+		$result{$value} =  1 + ($result{$value} // 0); 
 	}
-	return "6,8";
+	
+	return \%result;
+	
+}
+
+sub unhistogram {
+	my ($histogram) = @_;
+	
+	my @fishtypes = keys %$histogram;
+	
+	my @fishlist = map { ($_ ) x $$histogram{$_} } @fishtypes;
+	
+	return join ',', @fishlist;
+	
+}
+
+
+sub decrease {
+	my ($type, $count) = @_;
+	
+	my %result; 
+	
+	if ($type > 0) {
+		return ( [$type -1, $count]) ;
+	}
+	return ([6, $count],[8, $count]);
 }
 
 sub nextGeneration {
 	my ($generation) = @_;
 	
-	return join ',', map {decrease($_)} split ',', $generation;
+	my %nextgeneration; 
+	
+	while (my ($type, $count) = each %$generation) {
+		my @newmembers = decrease($type, $count);
+		foreach my $membertype (@newmembers) {
+			$nextgeneration{$membertype->[0]} = ($nextgeneration{$membertype->[0]} // 0) + $membertype->[1] 
+		} 
+	}
+	
+	return \%nextgeneration;
 	
 }
 
 sub generations {
 	my ($fishes, $gencount) = @_;
 	
+	my $fish_histogram = histogram($fishes);
+	
 	foreach my $counter ( 1..$gencount) {
-		$fishes = nextGeneration($fishes);
+			$fish_histogram = nextGeneration($fish_histogram);
 	}
 	
-	return $fishes;
+	return $fish_histogram;
 }
 
 
 sub fishCount {
-	my ($fishes) = @_;
+	my ($fish_histogram) = @_;
 	
-	return scalar split ',', $fishes; 
+	my $sum = 0;
+	foreach my $count (values %$fish_histogram) {
+		$sum += $count;
+	} 
+	
+	return $sum;
 }
+
 sub getInput() {
 	open (my $fh, '<', '../input') or die 'could not read input';
 	$/ = undef;
@@ -45,10 +89,15 @@ sub getInput() {
 }
 
 
-if (!caller(0)) {
-	my $finalFishCount = fishCount(generations(getInput(), 80));
+if (!caller(0)) {	
 	
-	say "After 80 generations, there will be $finalFishCount fishes";
+	my $input = getInput();
+	my %input = histogram($input);
+	my $gen80 = fishCount(generations($input, 80));
+	say "After 80 generations, there will be $gen80 fishes";
+	
+	my $gen256 = fishCount(generations($input, 256));
+	say "After 256 generations, there will be $gen256 fishes";
 	
 }
 1;
--- Puzzle6/perl/solution.test.pl
+++ Puzzle6/perl/solution.test.pl
@@ -7,10 +7,11 @@ use 5.32.0;
 
 require "solution.pl";
 
-is( nextGeneration('3,4,3,1,2'), '2,3,2,0,1');
-is(  nextGeneration('2,3,2,0,1'), '1,2,1,6,8,0');
-is( fishCount('2,3,2,0,1'), 5);
-is( generations('3,4,3,1,2', 2), '1,2,1,6,8,0');
+is_deeply( nextGeneration(histogram('3,4,3,1,2')), histogram('2,3,2,0,1'));
+is_deeply( nextGeneration(histogram('2,3,2,0,1')), histogram('1,2,1,6,8,0'));
+is( fishCount(histogram('2,3,2,0,1')), 5);
+is_deeply( generations('3,4,3,1,2', 2), histogram('1,2,1,6,8,0'));
 is( fishCount(generations("3,4,3,1,2", 80)), 5934);
+is( fishCount(generations("3,4,3,1,2", 256)), 26984457539);
 done_testing();
 1;
