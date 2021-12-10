8c47371466d1af06fe4b704b4be7bfb7c058922b
Stefan Denker <Stefan@dn-kr.de>
2021-12-10 21:50:32 +0100

    Puzzle 10: Part 1 completed

--- Puzzle10/perl/solution.pl
+++ Puzzle10/perl/solution.pl
@@ -6,6 +6,16 @@ use 5.32.0;
 use List::Util qw(reduce);
 use List::MoreUtils; 
 
+my %open_to_close = split '', '()[]{}<>';
+
+my %illegal_character_score = (
+	')' => 3,
+	']' => 57,
+	'}' => 1197,
+	'>' => 25137
+);
+
+
 sub legal {
 	my ($input) = @_;
 	
@@ -13,7 +23,7 @@ sub legal {
 	
 	my @stack =();
 	my $valid_until_here = 1;
-	my $index = 0;
+	my $additional_info = undef; 
 	do {
 		my $char = shift @inputs;
 		if ( $char =~ /[({\[<]/ ) {
@@ -21,13 +31,45 @@ sub legal {
 		} elsif (@stack){
 			my $previous = pop @stack; 
 			$valid_until_here = "$previous$char" =~ /^(?:\(\)|\[\]|\{\}|<>)$/;
+			if (!$valid_until_here) {
+				$additional_info = {'expected' => $open_to_close{$previous}, 'found' => $char };
+			}
 		} else {
 			return 0;
 		}
 	} while ($valid_until_here and scalar @inputs);
 	
-	return $valid_until_here;
+	return ($additional_info, $valid_until_here);
 }
 
+sub calculate_score {
+	my ($input) = @_;
+	
+	return $illegal_character_score{$input->{'found'}};
+}
+
+sub total_illegal_character_score {
+	
+	return reduce {$a + $b} 
+		map {my ($invalid) = legal($_); calculate_score($invalid)} 
+		grep { !legal($_) } 
+		@_;
+	
+}
+
+sub getInput() {
+	open (my $fh, '<', '../input') or die 'could not read input';
+	$/ = undef;
+	my $result = <$fh>;
+	close($fh);
+	return split "\n", $result;
+}
+
+if (!caller(0)) {	
+	my @input = getInput();
+	my $illegal_score = total_illegal_character_score(@input);
+	
+	say "Illegal score sum: $illegal_score";
+}
 1;
 
--- Puzzle10/perl/solution.test.pl
+++ Puzzle10/perl/solution.test.pl
@@ -6,6 +6,19 @@ use 5.32.0;
 use Test::More; 
 
 require_ok('./solution.pl');
+
+my @test_input = (
+"[({(<(())[]>[[{[]{<()<>>",
+"[(()[<>])]({[<{<<[]>>(",
+"{([(<{}[<>[]}>{[]{[(<()>",
+"(((({<>}<{<{<>}{[]{[]{}",
+"[[<[([]))<([[{}[[()]]]",
+"[{[{({}]{}}([{[{{{}}([]",
+"{<[[]]>}<{[{[{[]{()[[[]",
+"[<(<(<(<{}))><([]([]()",
+"<{([([[(<>()){}]>(<<{{",
+"<{([{{}}[<[[[<>{}]]]>[]]");
+
 foreach my $corrupted_line (
 		'{([(<{}[<>[]}>{[]{[(<()>',
 		'>', 
@@ -14,11 +27,15 @@ foreach my $corrupted_line (
 		'[<(<(<(<{}))><([]([]()',
 		'<{([([[(<>()){}]>(<<{{',
 	) {
-	ok (! scalar legal($corrupted_line), "input $corrupted_line is not legal");
+	my $ret = scalar legal($corrupted_line);
+	ok (! $ret, "input $corrupted_line is not legal");
 }
+my @result = legal('{([(<{}[<>[]}>{[]{[(<()>');
+is_deeply($result[0],{'expected' => ']', 'found' => '}'});
 
 foreach my $incomplete ('(((({<>}<{<{<>}{[]{[]{}') {
 	ok(legal($incomplete), "input $incomplete is legal");
 }
+is(total_illegal_character_score(@test_input), 26397);
 
 done_testing;
